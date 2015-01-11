fce5688c4ad97fca20fd7853fc64b5ef59f42bc0
blankwavercade <boyle.patrick.justin@gmail.com>
2015-01-11 12:57:39 -0500

    Prime Numbers Under N

---
+++ primeUnderN/prime.js
@@ -0,0 +1,26 @@
+var fs  = require("fs");
+var lines = [];
+var count;
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+  if (line != "") {
+    var primes = [];
+    var prime;
+    for(var i = 2; i < +line; i++) { 
+      prime = isPrime(i);
+      if(prime) { 
+        primes.push(prime); 
+      }
+    }
+    console.log(primes.join(','));
+  }
+});
+
+
+function isPrime(n) { 
+ for(var i = 2; i < n; i++) { 
+    if(n % i === 0 && i !== n) { 
+         return false;
+       }
+  }
+ return n;
+}
