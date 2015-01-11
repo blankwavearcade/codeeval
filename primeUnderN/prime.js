9a01a6bf793f9f589ecb1df5453e7bf2b2d835d0
blankwavercade <boyle.patrick.justin@gmail.com>
2015-01-11 14:12:09 -0500

    fixing a fuck up

--- primeUnderN/prime.js
+++ primeUnderN/prime.js
@@ -1,7 +1,26 @@
-var fs  = require("fs");
+re("fs");
+var lines = [];
+var count;
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
   if (line != "") {
-    console.log(line);
+    var primes = [];
+    var prime;
+    for(var i = 2; i < +line; i++) { 
+      prime = isPrime(i);
+      if(prime) { 
+        primes.push(prime); 
+      }
+    }
+    console.log(primes.join(','));
   }
 });
 
+
+function isPrime(n) { 
+  for(var i = 2; i < n; i++) { 
+    if(n % i === 0 && i !== n) { 
+      return false;
+    }
+  }
+  return n;
+}
